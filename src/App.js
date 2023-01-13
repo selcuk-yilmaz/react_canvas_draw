a1d72133e58b24cf05a121016fdd9788281feec4
selcuk-yilmaz <yilmax4@gmail.com>
2023-01-13 17:37:23 +0300

    useRef was added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-
+import React,{useRef} from 'react';
 import './App.css';
 import CanvasDraw from "react-canvas-draw";
 import map from "./assets/mapApi.jpg"
 
 function App() {
+const firstCanvas=useRef(null)
+const secondCanvas=useRef(null)
 
+const handleClick =()=>{
+  const data=firstCanvas.current.getSaveData();
+  console.log(data);
+  secondCanvas.current.loadSaveData(data);
+}
 
   return (
     <div>
-      <CanvasDraw brushRadius={3} imgSrc={map} />
-        
-        <CanvasDraw/>
+      <button onClick={handleClick}>Save Drawing</button>
+      <CanvasDraw
+        brushRadius={3}
+        brushColor="#130f40"
+        catenaryColor="red"
+        hideGrid={false}
+        style={{ border: "2px solid #000" }}
+        // imgSrc={map}
+
+        ref={firstCanvas}
+      />
+
+      <CanvasDraw hideGrid={true} disabled={true} ref={secondCanvas} />
     </div>
   );
 }
